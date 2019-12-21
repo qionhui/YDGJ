db77e69cb5a2c0ad9f7823fabc7c8f530dd9de8f
qionhui <40323604+qionhui@users.noreply.github.com>
2019-12-21 12:32:50 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 $(function () {
     $(".nav_hide").fadeOut(0);
+    $(".row1_left .carousel .text_ul li").hide();
+    $(".row1_right .content2").hide();
+    $(".row1_left .carousel .text_ul li").css("opacity",1);
     $("nav  .ul_none .flex_1").mouseover(function () {
         if($(window).width() <= 1000){
             $(".nav_hide ul").addClass("hide");
             $(".nav_hide ul:eq(" + $(this).index() + ")").removeClass("hide");
         }
+        else $(".nav_hide ul").removeClass("hide");
         $(this).addClass("show");
         $(".nav_hide ul:eq(" + $(this).index() + ")").addClass("show");
     })
@@ -31,7 +35,7 @@ $(function () {
 
 
     let a = new Banner("banner_ul");
-    a.switchTime = 2000;
+    a.switchTime = 5000;
     a.xzStop = "#banner .auto";
     a.setBtn("#banner .sele_btn","xz");
     a.setLeftRight("#banner .left_move","#banner .right_move");
@@ -42,6 +46,77 @@ $(function () {
         $("#banner .sele_btn a").removeClass("xz");
         $(this).addClass("xz");
         a.setNow($(this).index()+1);
+    })
+
+    let cutIndex = 0;
+    let oldCutIndex = 0;
+    let timer;
+    let isCutIng = false;
+    function startTimer() {
+        timer = setInterval(function () {
+            cutIndex++;
+            cut();
+        },3000);
+    }
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+    function cut() {
+        isCutIng = true;
+        if (cutIndex >= 5){
+            cutIndex = 0;
+        }
+        if(cutIndex < 0){
+            cutIndex = 4;
+        }
+            $(".row1_left .carousel .img_ul li:eq("+(oldCutIndex)+")").animate({
+                "opacity":0
+            },500);
+            $(".row1_left .carousel .text_ul li:eq("+(oldCutIndex)+")").hide();
+        $(".row1_left .carousel .img_ul li:eq("+cutIndex+")").animate({
+            "opacity":1
+        },function () {
+            isCutIng = false;
+        });
+        $(".row1_left .carousel .text_ul li:eq("+cutIndex+")").show();
+        oldCutIndex = cutIndex;
+    }
+    startTimer();
+    oldCutIndex = 4;
+    cut();
+
+    $(".row1_left .left_move").click(function () {
+        if (isCutIng)return;
+        cutIndex++;
+        cut();
+    })
+    $(".row1_left .right_move").click(function () {
+        if (isCutIng)return;
+        cutIndex--;
+        cut();
+    })
+    $(".row1_left").mouseover(function () {
+        stopTimer();
+    })
+    $(".row1_left").mouseleave(function () {
+        startTimer();
+    })
+
+    $(".row1_right .title a").hover(function () {
+        if($(window).width() < 770)return;
+        $(".row1_right .xz").removeClass("xz");
+        $(this).addClass("xz");
+        $(".row1_right .content .re>.flex").hide();
+        $(".row1_right .content .re>.flex").stop(true);
+        $(".row1_right .content .re>.flex:eq("+$(this).index()+")").fadeIn(500);
+    })
+    $(".row1_right .title a").click(function () {
+        if($(window).width() >= 770)return;
+        $(".row1_right .title a").toggleClass("xz");
+        $(".row1_right .content .re>.flex").hide();
+        $(".row1_right .content .re>.flex").stop(true);
+        $(".row1_right .content .re>.flex:eq("+$(this).index()+")").fadeIn(500);
     })
 
 })
